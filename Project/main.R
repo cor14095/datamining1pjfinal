7be0848cf02c1326aa314f723f0c37d4d7f73844
Alejandro Cortes <ale.crts1996@gmail.com>
2021-02-02 18:31:41 -0600

    Update format, clean code and finish DT

--- Project/main.R
+++ Project/main.R
@@ -221,8 +221,11 @@ rm(i)
 rm(varNameCount)
 rm(varPos)
 
-# Naive Bayes
-# Hip 1
+# -----------------------------------------------------------------------------
+# |                             Naive Bayes                                   |
+# -----------------------------------------------------------------------------
+
+# --------------------------------- Hip 1 -------------------------------------
 modeloBayes <- naiveBayes(Response ~ ., data = train)
 predNB_Test_1<-predict(modeloBayes, newdata=test, type="raw")
 
@@ -237,12 +240,10 @@ accuracyNB_Test_1 <- sum(diag(resultsNB_Test_1))/sum(resultsNB_Test_1)
 accuracyNB_Test_1
 
 # Recall
-# TP/(TP+FN)
 recall_Test_1 <- (resultsNB_Test_1[2,2]/(resultsNB_Test_1[2,1]+resultsNB_Test_1[2,2]))
 recall_Test_1
 
 # Precision
-# TP/(TP+FP)
 precision_Test_1 <- (resultsNB_Test_1[2,2]/(resultsNB_Test_1[1,2]+resultsNB_Test_1[2,2]))
 precision_Test_1
 
@@ -250,8 +251,9 @@ precision_Test_1
 rm(modeloBayes)
 rm(predictionNB_Test_1)
 rm(predNB_Test_1)
+# -------------------------------- Fin Hip 1 ----------------------------------
 
-# Hip 2
+# --------------------------------- Hip 2 -------------------------------------
 modeloBayes2 <- naiveBayes(Response ~ Age + Gender + Previously_Insured +
                              Vehicle_Age + Vehicle_Damage + Policy_Sales_Channel, 
                            method="class", 
@@ -271,12 +273,10 @@ accuracyNB_Test_2 <- sum(diag(resultsNB_Test_2))/sum(resultsNB_Test_2)
 accuracyNB_Test_2
 
 # Recall
-#TP/(TP+FN)
 recall_Test_2<-(resultsNB_Test_2[2,2]/(resultsNB_Test_2[2,1]+resultsNB_Test_2[2,2]))
 recall_Test_2
 
 # Precision
-#TP/(TP+FP)
 precision_Test_2<-(resultsNB_Test_2[2,2]/(resultsNB_Test_2[1,2]+resultsNB_Test_2[2,2]))
 precision_Test_2
 
@@ -284,12 +284,137 @@ precision_Test_2
 rm(modeloBayes2)
 rm(predictionNB_Test_2)
 rm(predNB_Test_2)
+# -------------------------------- Fin Hip 2 ----------------------------------
 
-# Decision Tree
+# -----------------------------------------------------------------------------
+# |                           Decision Tree                                   |
+# -----------------------------------------------------------------------------
 
-# Random Forest
+# --------------------------------- Hip 1 -------------------------------------
+modeloDT<-rpart(Response ~ ., method="class", data=train)
+prp(modeloDT) #gráfica básica 
+rpart.plot(modeloDT) #Gráfica recomendada
 
-# Logic Regression
+predictionDT_Test_1<-predict(modeloDT, newdata=test, type="class")
+predictionDT_Test_1 = as.character(predictionDT_Test_1)
+predictionDT_Test_1 = ifelse(predictionDT_Test_1 == 1, "SI", "NO")
+resultsDT_Test_1 <- table(test$Response, predictionDT_Test_1)
+resultsDT_Test_1
+prop.table(resultsDT_Test_1)
+
+# Acurracy
+accuracyDT_Test_3<-sum(diag(resultsDT_Test_1))/sum(resultsDT_Test_1)
+accuracyDT_Test_3
+
+# Recall
+recall_Test_3<-(resultsDT_Test_1[2,2]/(resultsDT_Test_1[2,1]+resultsDT_Test_1[2,2]))
+recall_Test_3
+
+# Precision
+precision_3<-(resultsDT_Test_1[2,2]/(resultsDT_Test_1[1,2]+resultsDT_Test_1[2,2]))
+precision_3
+
+# Clean Memory
+rm(modeloDT)
+rm(predictionDT_Test_1)
+rm(resultsDT_Test_1)
+# -------------------------------- Fin Hip 1 ----------------------------------
+
+# --------------------------------- Hip 2 -------------------------------------
+modeloDT2<-rpart(Response ~ Age + Gender + Previously_Insured +
+                  Vehicle_Age + Vehicle_Damage + Policy_Sales_Channel, 
+                method="class", data=train)
+prp(modeloDT2) #gráfica básica 
+rpart.plot(modeloDT2) #Gráfica recomendada
+
+predictionDT_Test_2<-predict(modeloDT2, newdata=test, type="class")
+predictionDT_Test_2 = as.character(predictionDT_Test_2)
+predictionDT_Test_2 = ifelse(predictionDT_Test_2 == 1, "SI", "NO")
+resultsDT_Test_2 <- table(test$Response, predictionDT_Test_2)
+resultsDT_Test_2
+prop.table(resultsDT_Test_2)
+
+# Acurracy
+accuracyDT_Test_4<-sum(diag(resultsDT_Test_2))/sum(resultsDT_Test_2)
+accuracyDT_Test_4
+
+# Recall
+recall_Test_4<-(resultsDT_Test_2[2,2]/(resultsDT_Test_2[2,1]+resultsDT_Test_2[2,2]))
+recall_Test_4
+
+# Precision
+precision_4<-(resultsDT_Test_2[2,2]/(resultsDT_Test_2[1,2]+resultsDT_Test_2[2,2]))
+precision_4
+
+# Clean Memory
+rm(modeloDT2)
+rm(predictionDT_Test_2)
+rm(resultsDT_Test_2)
+# -------------------------------- Fin Hip 2 ----------------------------------
+
+# -----------------------------------------------------------------------------
+# |                              Random Forest                                |
+# -----------------------------------------------------------------------------
+
+# --------------------------------- Hip 1 -------------------------------------
+
+# Acurracy
+
+# Recall
+
+# Precision
+
+# Clean Memory
+rm(modeloRF)
+rm(predictionRF_Test_1)
+rm(resultsRF_Test_1)
+# -------------------------------- Fin Hip 1 ----------------------------------
+
+# --------------------------------- Hip 2 -------------------------------------
+
+# Acurracy
+
+# Recall
+
+# Precision
+
+# Clean Memory
+rm(modeloRF2)
+rm(predictionRF_Test_2)
+rm(resultsRF_Test_2)
+# -------------------------------- Fin Hip 2 ----------------------------------
+
+# -----------------------------------------------------------------------------
+# |                            Logic Regression                               |
+# -----------------------------------------------------------------------------
+
+# --------------------------------- Hip 1 -------------------------------------
+
+# Acurracy
+
+# Recall
+
+# Precision
+
+# Clean Memory
+rm(modeloLR)
+rm(predictionLR_Test_1)
+rm(resultsLR_Test_1)
+# -------------------------------- Fin Hip 1 ----------------------------------
+
+# --------------------------------- Hip 2 -------------------------------------
+
+# Acurracy
+
+# Recall
+
+# Precision
+
+# Clean Memory
+rm(modeloLR2)
+rm(predictionLR_Test_2)
+rm(resultsLR_Test_2)
+# -------------------------------- Fin Hip 2 ----------------------------------
 
 
 #Tabla de comparacion Accuracy, Recall y Precision
@@ -306,17 +431,23 @@ modelo = c(
   )
 accuracy = c(
   accuracyNB_Test_1,
-  accuracyNB_Test_2
+  accuracyNB_Test_2,
+  accuracyDT_Test_3,
+  accuracyDT_Test_4
 )
 
 precision = c(
   precision_Test_1,
-  precision_Test_2
+  precision_Test_2,
+  precision_Test_3,
+  precision_Test_4
 )
 
 recall = c(
   recall_Test_1,
-  recall_Test_2
+  recall_Test_2,
+  recall_Test_3,
+  recall_Test_4
 )
 
 resumen = data.frame(modelo, accuracy, precision, recall)
